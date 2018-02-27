479e3b8acf277d07001f60d06dc6115d09a330d3
Fatih Akturk <f.akturk@gmail.com>
2018-02-27 17:12:58 +0900

    initial commit

--- bigLittleEndian.go
+++ bigLittleEndian.go
@@ -5,17 +5,41 @@ import (
     "github.com/gorilla/mux"
     "log"
     "net/http"
+    "bytes"
 )
+
+type BigLittleBinary struct{
+   bytes.Buffer bigEndian     `json:"bigendian,omitempty"`
+   bytes.Buffer littleEndian  `json:"littleendian,omitempty"`
+
+}
 // Display a single data
 func GetBinary(w http.ResponseWriter, r *http.Request) {
     params := mux.Vars(r)
+    number := params["number"]
+
+
+      bufLittle := new(bytes.Buffer)
+      bufBig := new(bytes.Buffer)
+
+    err := binary.Write(bufLittle, binary.LittleEndian, number)
+      if err != nil {
+            fmt.Println("Little Endian:", err)
+}
+
+err := binary.Write(bufBig, binary.BigEndian, number)
+  if err != nil {
+        fmt.Println("Little Endian:", err)
+}
+// bigLittleBinary := 
+
     // for _, item := range people {
     //     if item.ID == params["id"] {
     //         json.NewEncoder(w).Encode(item)
     //         return
     //     }
     // }
-    json.NewEncoder(w).Encode(&Person{})
+    json.NewEncoder(w).Encode(&BigLittleBinary{})
 }
 
 func main() {
